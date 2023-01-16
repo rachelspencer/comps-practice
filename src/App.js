8f378cc1bcb835f27343f6c152e32f2933976ec9
Rachel Spencer <rachelspencer688@gmail.com>
2023-01-16 15:21:10 -0500

    initial commit w/ custom button comp

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Button from './Button';
+
+const App = () => {
+    return (
+        <div>
+            <div>
+                <Button>Click Me</Button>
+            </div>
+            <div>
+                <Button>Buy Now!</Button>
+            </div>
+            <div>
+                <Button>See Deal!</Button>
+            </div>
+            <div>
+                <Button>Hide Ads!</Button>
+            </div>
+            <div>
+                <Button>Something!</Button>
+            </div>
+        </div>
+    )
+};
+
+export default App;
