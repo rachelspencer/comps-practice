d8d70a2a16fc1224e0aa5d9d9b5fff1d4c9693c3
Rachel Spencer <rachelspencer688@gmail.com>
2023-01-25 16:44:27 -0500

    arrow icon added, css added

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 const Accordion = ({ items }) => {
     const [expandIndex, setExpandeIndex ] = useState(0);
@@ -10,23 +11,23 @@ const Accordion = ({ items }) => {
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandIndex;
 
-        const icon = <span>
-            { isExpanded ? 'DOWN' : 'LEFT' }
-        </span>
+        const icon = (
+        <span className='text-xl'>{ isExpanded ? <GoChevronDown /> : <GoChevronLeft /> }</span>
+        );
         
         return (
             <div key={item.id}>
-                <div onClick={() => handleClick(index)}>
-                    {icon}
+                <div className='flex p-3 bg-gray-50 border-b items-center cursor-pointer justify-between' onClick={() => handleClick(index)}>
                     {item.label}
+                    {icon}
                 </div>
-                {isExpanded && <div>{item.content}</div>}
+                {isExpanded && <div className='border-b p-5'>{item.content}</div>}
             </div>
         );
     });
 
     return (
-        <div>{renderedItems}</div>
+        <div className='border-x border-t rounded'>{renderedItems}</div>
     );
 };
 
