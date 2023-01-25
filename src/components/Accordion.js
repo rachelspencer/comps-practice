205506f984e5e935044359ebddcf6973de6fc2ce
Rachel Spencer <rachelspencer688@gmail.com>
2023-01-25 16:26:39 -0500

    example of event handler outside mapping statement

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,12 +3,16 @@ import { useState } from 'react';
 const Accordion = ({ items }) => {
     const [expandIndex, setExpandeIndex ] = useState(0);
 
+    const handleClick = (nextIndex) => {
+        setExpandeIndex(nextIndex);
+    };
+
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandIndex;
         
         return (
             <div key={item.id}>
-                <div>{item.label}</div>
+                <div onClick={() => handleClick(index)}>{item.label}</div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
         );
