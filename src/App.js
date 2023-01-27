32b4b8062323bed9f02f2d8c12fa232807aacdf3
Rachel Spencer <rachelspencer688@gmail.com>
2023-01-27 10:55:54 -0500

    DropdownPage in pages created

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import { useState } from 'react';
 
-import Dropdown from './components/Dropdown';
+// import  from './components/';
 
 
 const App = () => {
-    const [ selection, setSelection ] = useState(null);
-
-    const handleSelect = (option) => {
-        setSelection(option);
-    };
-
-    const options = [
-        {label: 'Red', value: 'red'},
-        {label: 'Green', value: 'green'},
-        {label: 'Blue', value: 'blue'},
-    ];
-
-    return (
-        <div className="flex">
-            <Dropdown 
-                options={options} 
-                value={selection} 
-                onChange={handleSelect}
-            />
-            <Dropdown 
-                options={options} 
-                value={selection} 
-                onChange={handleSelect}
-            />
-        </div>
-    );
+    return <div></div>;
 };
 
 export default App;
