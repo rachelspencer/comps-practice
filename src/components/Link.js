6431cc7ea74cd13a9c69b6de5bbbe82710444ce1
Rachel Spencer <rachelspencer688@gmail.com>
2023-01-27 13:19:43 -0500

    custom nav hook created

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,17 +1,23 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import classNames from 'classnames';
+import useNavigation from "../hooks/use-navigation";
 
 const Link = ({ to, children }) => {
-    const { navigate } = useContext(NavigationContext);
+    const { navigate } = useNavigation();
+
+    const classes = classNames('text-blue-500');
 
     const handleClick = (event) => {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        };
+        
         event.preventDefault();
 
         navigate(to);
     };
 
     return (
-        <a onClick={handleClick}>{children}</a>
+        <a className={classes} href={to} onClick={handleClick}>{children}</a>
     );
 };
 
