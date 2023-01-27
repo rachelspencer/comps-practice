4f665a3f87332f960913aa0708807e6329244969
Rachel Spencer <rachelspencer688@gmail.com>
2023-01-27 12:36:45 -0500

    Route component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-//import { useState } from 'react';
-
-// import  from './components/';
-
+import Link from './components/Link';
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
 
 const App = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Link to="/accordion">Go to accordion</Link>
+            <Link to="/dropdown">Go to dropdown</Link>
+            <div>
+                <Route path="/accordion">
+                    <AccordionPage />
+                </Route>
+                <Route path="/dropdown">
+                    <DropdownPage />
+                </Route>
+            </div>
+        </div>
+    );
 };
 
 export default App;
